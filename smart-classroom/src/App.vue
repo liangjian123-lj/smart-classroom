97df7c18ff8b4301ab54179f154d59a2b9592583
Elaine <1807332757@qq.com>
2020-12-17 15:04:01 +0800

    style: 侧边导航栏

--- smart-classroom/src/App.vue
+++ smart-classroom/src/App.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="app">
     <header-bar></header-bar>
-    <router-view/>
+    <main-content class="margin-top"></main-content>
   </div>
 </template>
 
 <script>
   import HeaderBar from "./components/HeaderBar";
+  import MainContent from "./components/MainContent";
 export default {
   name: 'App',
   components: {
-    HeaderBar
+    HeaderBar,
+    MainContent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background: #F6F6F6;
+  }
+  .margin-top{
+    margin-top: 20px;
+  }
 </style>
---
+++ smart-classroom/src/components/MainContent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="contain">
+    <div class="nav-side">
+      <navigation></navigation>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Navigation from "./Navigation";
+  export default {
+    name: "MainContent.vue",
+    components: {
+      Navigation
+    }
+  }
+</script>
+
+<style scoped>
+  .contain{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+  }
+  .nav-side{
+    width: 1280px;
+    background: #f6f6f6;
+  }
+</style>
