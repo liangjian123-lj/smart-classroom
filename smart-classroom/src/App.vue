68b419eef8df571f9908aab29f13cfcf7e31ba20
Elaine <1807332757@qq.com>
2020-12-20 23:46:32 +0800

    style: 创建首页

--- smart-classroom/src/App.vue
+++ smart-classroom/src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
-    <header-bar></header-bar>
-    <main-content class="margin-top"></main-content>
+    <!-- 教职工会员中心页面   -->
+    <template v-if="$route.meta.showNav">
+      <header-bar></header-bar>
+      <main-content class="margin-top"></main-content>
+    </template>
+    <!-- 平台门户页面   -->
+    <template v-else>
+      <index></index>
+    </template>
   </div>
 </template>
 
 <script>
   import HeaderBar from "./components/HeaderBar";
   import MainContent from "./components/MainContent";
+  import Index from "./views/Index";
 export default {
   name: 'App',
   components: {
     HeaderBar,
-    MainContent
+    MainContent,
+    Index
   }
 }
 </script>
--- smart-classroom/src/router/index.js
+++ smart-classroom/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import VedioManagement from "../components/VedioManagement";
+import Index from "../views/Index";
 
 Vue.use(Router)
 
@@ -8,8 +9,16 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Index',
+      component: Index,
+    },
+    {
+      path: '/vedioManagement',
       name: 'VedioManagement',
-      component: VedioManagement
+      component: VedioManagement,
+      meta:{
+        showNav: true
+      }
     },
 
   ]
