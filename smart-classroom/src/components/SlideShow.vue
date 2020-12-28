0bbea23ca10c74d6c28e5359e40c4b40ac2e7e3f
Liangjian <1346989102@qq.com>
2020-12-28 11:02:36 +0800

    style: index部分样式

---
+++ smart-classroom/src/components/SlideShow.vue
@@ -0,0 +1,53 @@
+<template >
+  <a-carousel autoplay :style="{width:'1180px',height:'160px',margin:'0 auto'}">
+    <template v-for="(item,index) in imgList">
+      <div ><img :src="item.src" :alt="item.name" class="img"></div>
+    </template>
+  </a-carousel>
+</template>
+<script>
+import banner from '../assets/banner.png'
+export default {
+  name: "SlideShow",
+  data() {
+    return {
+      imgList: [
+        {
+          src: banner,
+          name: 'img1'
+        },
+        // {
+        //   src: avatar,
+        //   name: 'img1'
+        // },
+        // {
+        //   src: avatar,
+        //   name: 'img1'
+        // },
+        // {
+        //   src: avatar,
+        //   name: 'img1'
+        // },
+      ]
+    }
+  }
+
+};
+</script>
+<style scoped>
+/* For demo */
+.ant-carousel >>> .slick-slide {
+  text-align: center;
+  height: 158px;
+  line-height: 160px;
+  background: #364d79;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.ant-carousel >>> .slick-slide h3 {
+  color: #fff;
+}
+</style>
+
+
--- smart-classroom/src/views/Index.vue
+++ smart-classroom/src/views/Index.vue
@@ -1,15 +1,70 @@
 <template>
   <div>
-    我是首页
+    <header-bar></header-bar>
+    <div class="banner" >
+<!--      <slide-show></slide-show>-->
+    </div>
+    <div id="components-layout-demo-basic" :style="{marginTop:'19px'}">
+      <a-layout :style="{width:'1180px',margin:'0 auto',}">
+        <a-layout>
+          <a-layout-header :style="{marginRight:'20px'}">Header</a-layout-header>
+          <a-layout-content :style="{marginRight:'20px'}">Content</a-layout-content>
+
+        </a-layout>
+        <a-layout-sider>Sider</a-layout-sider>
+      </a-layout>
+      <a-layout-footer>Footer</a-layout-footer>
+
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Index.vue"
+import HeaderBar from "../components/HeaderBar";
+// import SlideShow from "../components/SlideShow";
+export default {
+  name: "Index.vue",
+  components: {
+    // SlideShow,
+    HeaderBar
   }
+}
 </script>
 
 <style scoped>
+.banner{
+  margin-top:20px;
+}
+#components-layout-demo-basic {
 
+  text-align: center;
+}
+#components-layout-demo-basic .ant-layout-header,
+#components-layout-demo-basic .ant-layout-footer {
+  background: #7dbcea;
+  color: #fff;
+}
+#components-layout-demo-basic .ant-layout-footer {
+  line-height: 1.5;
+}
+#components-layout-demo-basic .ant-layout-sider {
+  background: #3ba0e9;
+  color: #fff;
+  line-height: 120px;
+}
+#components-layout-demo-basic .ant-layout-content {
+  background: rgba(16, 142, 233, 1);
+  color: #fff;
+  min-height: 120px;
+  line-height: 120px;
+}
+#components-layout-demo-basic > .ant-layout {
+  margin-bottom: 48px;
+}
+#components-layout-demo-basic > .ant-layout:last-child {
+  margin: 0;
+}
 </style>
+
+
+
